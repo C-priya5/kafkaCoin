b21ff85f9b5294a045be421fba8d78f90f7dd2aa
C-priya5 <151828786+C-priya5@users.noreply.github.com>
2024-08-03 12:44:23 +0530

    added producer code

---
+++ producer.py
@@ -0,0 +1,27 @@
+import requests
+from kafka import KafkaProducer
+import time
+import json
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+url = 'https://api.coinbase.com/v2/prices/spot?currency=USD'
+
+def fetch_coinbase_data():
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+while True:
+    data = fetch_coinbase_data()
+    if data:
+        producer.send('coinbase', data)
+        print(f"Sent: {data}")
+    time.sleep(10)  # Adjust the frequency as needed
+
+producer.close()
